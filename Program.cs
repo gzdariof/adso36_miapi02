05eec2140ec7155b95ef9de7b0ab30bbe92eea00
Dario F. Gomez Z. <gz.dariof@gmail.com>
2026-09-10 20:33:15 -0500

    Creacion de API y agrego servicio de Swagger

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MiApi02",
+        Version = "v1",
+        Description = "API de ejemplo en .NET 10"
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "MiApi02 v1");
+        options.RoutePrefix = string.Empty; // Swagger en la raíz
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
